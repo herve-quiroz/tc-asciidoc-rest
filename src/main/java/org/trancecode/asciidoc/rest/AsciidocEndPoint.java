58b327389551daf16400eb3e1af8b56c1960b16e
Herve Quiroz <herve.quiroz@trancecode.org>
2012-12-18 00:02:00 +0000

    Add a cache for the generated output

--- src/main/java/org/trancecode/asciidoc/rest/AsciidocEndPoint.java
+++ src/main/java/org/trancecode/asciidoc/rest/AsciidocEndPoint.java
@@ -16,13 +16,23 @@
 package org.trancecode.asciidoc.rest;
 
 import com.google.common.base.Preconditions;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.Maps;
+import com.google.common.io.ByteStreams;
 import com.google.common.io.Closeables;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URLConnection;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -50,26 +60,38 @@ public final class AsciidocEndPoint extends HttpServlet
         return URI.create(connection.getHeaderField("Location"));
     }
 
-    private static InputStream get(final URI uri, final boolean followRedirect) throws IOException
+    private static URLConnection connectFollowRedirect(final URI uri) throws IOException
     {
-        LOG.trace("{@method} uri = {} ; followRedirect = {}", uri, followRedirect);
+        LOG.trace("{@method} uri = {}", uri);
         final HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
-        connection.setInstanceFollowRedirects(true);
         try
         {
-            if (followRedirect && isRedirect(connection.getResponseCode()))
+            if (isRedirect(connection.getResponseCode()))
             {
                 final URI redirectUri = getRedirect(connection);
                 LOG.trace("  redirect = {}", redirectUri);
-                return get(redirectUri, followRedirect);
+                return connectFollowRedirect(redirectUri);
             }
-            return uri.toURL().openStream();
+            return uri.toURL().openConnection();
         }
         finally
         {
             connection.disconnect();
         }
     }
+
+    private static Map.Entry<String, byte[]> getResource(final URI uri) throws IOException
+    {
+        LOG.trace("{@method} uri = {}", uri);
+        final URLConnection connection = connectFollowRedirect(uri);
+        final String etag = connection.getHeaderField("Etag");
+        LOG.trace("  etag = {}", etag);
+        final byte[] content = readBytesAndClose(connection.getInputStream());
+        LOG.trace("  content = {}", content.length);
+        return Maps.immutableEntry(etag, content);
+    }
+
+    private static final Cache<String, byte[]> CACHE = CacheBuilder.newBuilder().softValues().build();
 
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException,
@@ -80,23 +102,68 @@ public final class AsciidocEndPoint extends HttpServlet
         Preconditions.checkArgument(targetType.equals("/html"), "%s", targetType);
         final String path = request.getPathInfo();
         LOG.trace("  path = {}", path);
-        final URI targetUri = URI.create("http:/" + path);
-        LOG.trace("  targetUri = {}", targetUri);
-        LOG.debug("{} :: GET :: {} :: {}", request.getRemoteHost(), request.getRequestURI(), targetUri);
-        InputStream targetIn = null;
-        OutputStream htmlOut = null;
+        final URI sourceDocumentUri = URI.create("http:/" + path);
+        LOG.trace("  sourceDocumentUri = {}", sourceDocumentUri);
+        LOG.debug("{} :: GET :: {} :: {}", request.getRemoteHost(), request.getRequestURI(), sourceDocumentUri);
+        final Map.Entry<String, byte[]> sourceDocument = getResource(sourceDocumentUri);
+        final byte[] htmlOutput;
         try
         {
-            targetIn = get(targetUri, true);
-            Preconditions.checkState(targetIn != null);
-            htmlOut = response.getOutputStream();
-            AsciiDoc.toXhtml(targetIn, htmlOut);
+            htmlOutput = CACHE.get(sourceDocument.getKey(), new Callable<byte[]>()
+            {
+                @Override
+                public byte[] call() throws Exception
+                {
+                    InputStream targetIn = null;
+                    ByteArrayOutputStream htmlOut = null;
+                    try
+                    {
+                        targetIn = new ByteArrayInputStream(sourceDocument.getValue());
+                        Preconditions.checkState(targetIn != null);
+                        htmlOut = new ByteArrayOutputStream();
+                        AsciiDoc.toXhtml(targetIn, htmlOut);
+                    }
+                    finally
+                    {
+                        Closeables.closeQuietly(targetIn);
+                        Closeables.closeQuietly(htmlOut);
+                    }
+                    return htmlOut.toByteArray();
+                }
+            });
+        }
+        catch (final ExecutionException e)
+        {
+            throw new ServletException(sourceDocumentUri.toString(), e);
+        }
+        response.setContentType("application/xhtml+xml");
+        writeBytesAndClose(htmlOutput, response.getOutputStream());
+    }
+
+    private static byte[] readBytesAndClose(final InputStream in) throws IOException
+    {
+        try
+        {
+            return ByteStreams.toByteArray(in);
         }
         finally
         {
-            Closeables.closeQuietly(targetIn);
-            Closeables.closeQuietly(htmlOut);
+            Closeables.closeQuietly(in);
         }
-        response.setContentType("application/xhtml+xml");
+    }
+
+    private static void writeBytesAndClose(final byte[] bytes, final OutputStream out) throws IOException
+    {
+        InputStream in = null;
+        try
+        {
+            in = new ByteArrayInputStream(bytes);
+            ByteStreams.copy(in, out);
+        }
+        finally
+        {
+            Closeables.closeQuietly(in);
+            Closeables.closeQuietly(out);
+        }
     }
 }
